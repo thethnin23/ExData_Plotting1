039520dfc709e77ec941d41e853644f2f5769967
Thet Hnin Aye <thethnin23@gmail.com>
2015-07-11 16:01:28 +0630

    Exploratory Data Analysis Course Project 1

---
+++ Plot1.R
@@ -0,0 +1,5 @@
+data <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.string = "?")
+test <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+hist(test$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+dev.copy(png, filename="Plot1.png",width = 480, height= 480, units ="px")
+dev.off()
---
+++ Plot2.R
@@ -0,0 +1,7 @@
+data <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.string = "?")
+test <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+newDateTime <- strptime(paste(test$Date, test$Time), format = "%d/%m/%Y %H:%M:%S")
+newtest <- data.frame(test, DateTime = newDateTime)
+with(newtest, plot(DateTime, Global_active_power, type="l", xlab = "", ylab = "Global Active Power(kilowatts)"))
+dev.copy(png, filename="Plot2.png", width = 480, height= 480, units ="px")
+dev.off()
